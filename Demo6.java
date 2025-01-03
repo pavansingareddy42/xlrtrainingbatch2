ce8ab387e84c6e9ef1cdb832d0cd95fdc36b69aa
pavansingareddy42 <pavansingareddy42@gmail.com>
2025-01-03 10:11:04 +0530

    java code for user name and password verification

---
+++ Demo6.java
@@ -0,0 +1,11 @@
+
+public class Demo6{
+    public static void main(String[] args) {
+        String name = "pavan";
+        String Password = "pavan@123";
+        while(name == "pavan" && Password == "pavan@123"){
+            System.out.println("welcome to pavan");
+            break;
+        }
+    }
+}
