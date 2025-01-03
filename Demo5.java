5da325676b6764391572e5585b8a6108a308721a
pavansingareddy42 <pavansingareddy42@gmail.com>
2025-01-03 09:53:48 +0530

    Add files via upload
    java code for verify user name and password

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+
+public class Demo5{
+    public static void main(String[] args) {
+        String name = "pavan";
+        String Password = "pavan@123";
+        while(name == "pavan" && Password == "pavan@123"){
+            System.out.println("hello happy new year");
+            break;
+        }
+    }
+}
