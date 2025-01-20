d67b000f215f1adb53c72abbaf6e67562ca787de
pavansingareddy42 <pavansingareddy42@gmail.com>
2025-01-20 10:18:08 +0530

    SAMPLE ARRAY

---
+++ Demo029.java
@@ -0,0 +1,10 @@
+public class Demo029 {
+    public static void main(String[] args) {
+                String[] names = {"pavan","Bobby","viggu","goutham","sunny","rohith","aditya","kumar","imran","jathara"
+        };
+        System.out.println("The names in the array are:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
