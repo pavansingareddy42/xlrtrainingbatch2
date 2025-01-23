f0433334e4682bad6adda5d1c949451eb2c69b36
pavansingareddy42 <pavansingareddy42@gmail.com>
2025-01-23 09:50:48 +0530

    LC003

---
+++ LC003.java
@@ -0,0 +1,7 @@
+public class LC003 {
+    public static void main(String[] args) {
+        String str = "8790";
+        int num = Integer.parseInt(str);
+        System.out.println("String to int: " + num);
+    }
+}
