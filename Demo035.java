1cc8638d109077bd6b03395b3cef1d76af936f45
pavansingareddy42 <pavansingareddy42@gmail.com>
2025-01-25 10:18:39 +0530

    Demo038

---
+++ Demo035.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Demo035 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter another number: ");
+        int num2 = sc.nextInt();
+        if(num1 > num2){
+            System.out.println(num1 + " is greater than " + num2);
+        }
+        else if(num1 < num2){
+            System.out.println(num1 + " is less than " + num2);
+        
+    }
+    else{
+        System.out.println("num1 and num2 are equal");
+    }
+}
+}
