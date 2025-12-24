d009ffa31e9d953694ea0fb02f82c956354ad2b0
pavansingareddy42 <pavansingareddy42@gmail.com>
2025-12-24 09:59:16 +0530

    Add files via upload

---
+++ Assignmentoperator.java
@@ -0,0 +1,12 @@
+public class Assignmentoperator{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        int d=a-b;
+        System.out.println(d);
+        System.out.println(a*b);
+        System.out.println(a/d);
+       
+    }
+}
